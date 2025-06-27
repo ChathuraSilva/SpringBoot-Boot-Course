1fa47e525e33028a67534f3357bdd05ea36fccdd
Chathura Silva <chathurasilva18@gmail.com>
2025-06-27 20:27:44 +0530

    week 1 - intro

---
+++ laptopstroe/src/main/java/com/elearning/laptopstroe/LaptopstroeApplication.java
@@ -0,0 +1,13 @@
+package com.elearning.laptopstroe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaptopstroeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaptopstroeApplication.class, args);
+	}
+
+}
---
+++ laptopstroe/src/test/java/com/elearning/laptopstroe/LaptopstroeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elearning.laptopstroe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaptopstroeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
